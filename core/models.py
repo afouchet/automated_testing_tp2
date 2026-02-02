7d76db91e341ada994c821caa5f4ad75aea9cb4c
Arnaud Fouchet <foucheta@gmail.com>
2026-02-02 14:50:22 +0100

    More comments

--- core/models.py
+++ core/models.py
@@ -2,9 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+# Notre modèle d'utilisateur
 class BookUser(models.Model):
-    # For authentification
+    # Le modèle User built-in for authentification
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # On n'hérite pas du modèle en le super-chargeant:
+    # On créerait un super objet avec plein de rôle
+    #
+    # Là, il y a le Django User pour gérer l'authentification
+    # Et noter BookUser pour gérer toutes les informations qu'on veut
+    # enregistrer sur l'utilisateur
     is_company = models.BooleanField(default=False)
 
     @property
